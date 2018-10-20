ac8449966feb87f63bb8bb8fd1061402ede2cc3d
wendlingd <wendlingd@users.noreply.github.com>
2018-10-20 13:29:30 -0400

    Updated to run with 03_Fuzzy_match.py, HTML pgs

--- django/loganalysis/assignment/models.py
+++ django/loganalysis/assignment/models.py
@@ -1,7 +1,13 @@
+"""
+https://docs.djangoproject.com/en/2.1/topics/db/queries/
+"""
+
+
 from django.db import models
 
 class Assignment(models.Model):
     """ORM class for assignment table"""
+#    id = models.IntegerField(primary_key=True, null=False) # added to orig
     adjustedQueryCase = models.CharField(max_length=200, null=True, blank=True)
     NewSemanticTypeName = models.CharField(max_length=100, null=True, blank=True)
     preferredTerm = models.CharField(max_length=200, null=True, blank=True)
@@ -21,11 +27,13 @@ class Assignment(models.Model):
                         'Human':'Human',
                         'Journal Name':'Journal-related',
                         'Add to RegEx':'Add Regex',
+                        'Foreign':'Foreign Unresolved',
                         'Combo':'Combo'}
         if action in ['NLM Product/Service',
                             'Human', 
                             'Journal Name', 
                             'Add to RegEx', 
+                            'Foreign',
                             'Combo']:
             self.NewSemanticTypeName = user_to_db[action]
             self.preferredTerm = None
@@ -34,28 +42,27 @@ class Assignment(models.Model):
         elif action == 'Accept':
             self.NewSemanticTypeName = self.SemanticTypeName
         elif action == 'Foreign':
-            self.preferredTerm = 'Foreign language'
-            self.NewSemanticTypeName = 'Foreign language'
-            self.SemanticGroup = 'Foreign language'
+            self.preferredTerm = 'Foreign unresolved'
+            self.NewSemanticTypeName = 'Foreign unresolved'
+            self.SemanticGroup = 'Foreign unresolved'
         else:
             raise Exception("New type not recognized")
         self.save()
 
+    # For http://localhost:8000/fuzzy/fuzzyVetter
     def take_vetting_action(self, action):
         """Update fuzzy match details with user input"""        
         if action == 'Okay':
-            self.Modified = 0
+            self.Modified = 1
         elif action == 'Drop Row':
             self.delete()
             return
         elif action == 'Drop Preferred':
-            self.Modified = 1
+            self.Modified = 1 # Signifier for, this assignment was approved
             self.preferredTerm = None
         else:
             raise Exception("New type not recognized")
         self.save()
-
-
 
 
 def summarize():
@@ -66,11 +73,17 @@ def summarize():
     return results
 
 
+# For http://localhost:8000/fuzzy/
 def get_fuzzy_matches(limit=50):
     """Fetch matches needing user review"""
-    results = Assignment.objects.filter(NewSemanticTypeName=None). \
+    results = Assignment.objects.filter(NewSemanticTypeName=''). \
                 order_by('-timesSearched').all()[:limit]
     return results
+
+'''
+Previously: results = Assignment.objects.filter(NewSemanticTypeName=None). \
+Entry.objects.filter(blog__name='Beatles Blog')
+'''
 
 
 def create_form_from_object(obj, form):
@@ -78,10 +91,16 @@ def create_form_from_object(obj, form):
     return form({'query_id':obj.id})
 
 
+# For http://localhost:8000/fuzzy/fuzzyVetter
 def get_vetter_matches(limit=50):
     """Fetch fuzzy matches for user review"""
-    results = Assignment.objects.filter(Modified=None). \
+    results = Assignment.objects.exclude(NewSemanticTypeName__isnull=True). \
                 exclude(NewSemanticTypeName='Combo'). \
-                order_by('preferredTerm').all()[:limit]
+                exclude(NewSemanticTypeName=''). \
+                exclude(NewSemanticTypeName='Ignore'). \
+                exclude(Modified=1). \
+                order_by('-timesSearched').all()[:limit]
     return results
 
+# Was results = Assignment.objects.filter(Modified=None). \
+# order_by('preferredTerm').all()[:limit]
